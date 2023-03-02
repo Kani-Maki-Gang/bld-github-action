65e5fa402e5d61e4419dd5ae54a144ed0c9a9145
Kostas Vlachos <kvl_93@outlook.com>
2023-03-03 00:01:55 +0200

    Added new inputs for server, variables and environment.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { setFailed, getInput, error } from '@actions/core';
+import { setFailed, getInput, error, getMultilineInput } from '@actions/core';
 import { logMessagePrefix } from 'src/base/log';
 import { BinaryDownloader } from 'src/downloader/downloader';
 import { Runner } from 'src/runner/runner';
@@ -8,8 +8,12 @@ async function main() {
     const downloader = new BinaryDownloader();
     await downloader.download();
 
-    const pipeline = getInput('pipeline');
-    const runner = new Runner(pipeline);
+    const server: string | null = getInput('server');
+    const pipeline: string = getInput('pipeline');
+    const variables: string[] | null = getMultilineInput('variables');
+    const environment: string[] | null = getMultilineInput('environment');
+
+    const runner = new Runner(server, pipeline, variables, environment);
     await runner.run();
   } catch (err) {
     const message = `Failed to execute pipeline. ${err}`;
--- src/runner/runner.ts
+++ src/runner/runner.ts
@@ -3,13 +3,53 @@ import { debug } from "@actions/core";
 import { spawn } from "child_process";
 import { Definitions } from "src/base/definitions";
 
+export class Variable {
+  constructor(public name: string, public value: string) { }
+}
+
 export class Runner {
-  constructor(private pipeline: string) { }
+  constructor(
+    private server: string | null,
+    private pipeline: string,
+    private variables: string[] | null,
+    private environment: string[] | null,
+  ) { }
+
+  private getVariables(): string[] | null {
+    return this.variables?.reduce((acc, n) => acc.concat(['-v', n]), new Array<string>()) ?? null;
+  }
+
+  private getEnvironment(): string[] | null {
+    return this.environment?.reduce((acc, n)=> acc.concat(['-e', n]), new Array<string>()) ?? null;
+  }
+
+  private getArguments(): string[] {
+    let args = ['run'];
+
+    if (this.server) {
+      args.push('-s', this.server);
+    }
+
+    args.push('-p', this.pipeline);
+
+    const variables = this.getVariables();
+    if (variables) {
+      args = args.concat(variables);
+    }
+
+    const environment = this.getEnvironment();
+    if (environment) {
+      args = args.concat(environment);
+    }
+
+    return args;
+  }
 
   run(): Promise<void> {
     return new Promise((resolve, reject) => {
       debug(`starting run for pipeline: ${this.pipeline}`);
-      const handle = spawn(`${Definitions.binaryPath}/bld`, ['run', '-p', this.pipeline]);
+      const args = this.getArguments();
+      const handle = spawn(`${Definitions.binaryPath}/bld`, args);
       handle.stdout.on('data', data => console.log(data.toString()));
       handle.stderr.on('data', data => console.log(data.toString()));
       handle.on('exit', (code, _signal) => {
