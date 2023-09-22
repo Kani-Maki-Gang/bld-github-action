960832e9a6ac4be07078a5419298cf9390d07ff6
Kostas Vlachos <kvl_93@outlook.com>
2023-09-22 22:13:41 +0300

    Updated bld config and removed extra command from runner.

--- src/runner/runner.ts
+++ src/runner/runner.ts
@@ -62,7 +62,6 @@ export class Runner {
   async run() {
     debug(`starting run for pipeline: ${this.pipeline}`);
     const args = this.getArguments();
-    await this.spawn("cat", [".bld/config.yaml"]);
     await this.spawn(Definitions.binaryName, args);
   }
 }
